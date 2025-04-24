242d66091b24c68c57e82018c02c50c08121308e
Pedro Avila <pedroavila@Pedros-Laptop.local>
2025-04-24 14:10:10 -0600

    deleating hiden falls

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- /Users/pedroavila/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/pedroavila/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
